a8688e6c38f94f41c8ff547477b743ae9ae38f3b
Diana Marrufo <diana.marrufo@etpay.com>
2023-02-02 14:02:54 -0600

    no found message show

--- src/store/home/homeService.ts
+++ src/store/home/homeService.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
+import { ErrorCode } from '../commonTypes';
 
 const API_CITIES = 'https://search.reservamos.mx/api/v2/places?q=mon'
 const API_WEATHER = 'https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly&units=metric&lang=es&appid=a5a47c18197737e8eeca634cd6acb581'
 
 const getCities = async(userData: string) => {
   const response = await axios.get(`${API_CITIES}?q=${userData}`)
-  console.log('response',response)
   if (response.data.length >= 0) {
     return response.data[0]
   }
